3e1f3fce1adfd940d1d4cadd0af4b434e7a75ead
Ken Allman <ken.allman@practicelink.com>
2025-09-13 16:37:54 -0500

    Fix blank page by restoring proper Layout component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -16,10 +16,10 @@ export function Layout({ children, breadcrumbs = [], showSidebar = true }: Layou
     <div className="min-h-screen bg-gray-50">
       <Header />
       
-      <div className="flex min-h-0">
+      <div className="flex">
         {showSidebar && <Sidebar />}
         
-        <main className={`flex-1 bg-gray-50 ${showSidebar ? 'ml-0' : ''}`}>
+        <main className="flex-1">
           {breadcrumbs.length > 0 && (
             <div className="bg-white border-b border-gray-200 px-6 py-4">
               <Breadcrumb items={breadcrumbs} />
